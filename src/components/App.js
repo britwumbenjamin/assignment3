f647954ee1b3d276f1b8a79f388e24d993421ada
Benjamin Britwum <britwumbenjamin>
2021-05-29 15:42:15 +0000

    three

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Home from './components/home'
+import Form from './components/form'
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Form/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
